12e23abd38ee25199a3dff3559f09e8fd9cde0a1
Joeyscat <zhouyu.fun@gmail.com>
2020-06-22 23:50:06 +0800

    ch02

---
+++ go/src/jvmgo/ch02/classpath/classpath.go
@@ -0,0 +1,38 @@
+package classpath
+
+import "path/filepath"
+
+type Classpath struct {
+	bootClasspath Entry
+	extClasspath  Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+
+}
+
+func (self *Classpath) String() string {
+
+}
+
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	// jre/lib/*
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	// jre/lib/ext/*
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func getJreDir(jreOption string) string {
+
+}
